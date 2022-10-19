3d2dfb113518c27ded96698c231bb82f4b6d23da
alexbuyan <sashabuyan@yandex.ru>
2022-10-19 14:47:36 +0300

    adding login and register

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,18 +1,48 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Register.module.scss";
 
 export default function Register() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+
+  const addUser = () => {
+    if (login !== "" && password !== "") {
+      setLogin(login);
+      setPassword(password);
+      let path = "/";
+      navigate(path);
+    }
+  };
+
   return (
     <div className={styles.body}>
       <div className={styles.inputs}>
-        <input type="text" placeholder="login" className={styles.inputLogin} />
         <input
           type="text"
-          placeholder="register"
+          placeholder="login"
+          className={styles.inputLogin}
+          name="login"
+          value={login}
+          onChange={(e) => {
+            setLogin(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="password"
           className={styles.inputRegister}
+          name="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
         />
       </div>
-      <button className={styles.registerButton}>Зарегистрироваться</button>
+      <button className={styles.registerButton} onClick={addUser}>
+        Зарегистрироваться
+      </button>
     </div>
   );
 }
