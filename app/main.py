4783cf3703e3c665ae1da1f8a77259b6f64f2107
Slava314 <vorobev.stu@gmail.com>
2022-10-19 13:27:22 +0300

    add fastapi

---
+++ app/main.py
@@ -0,0 +1,15 @@
+import time
+
+from fastapi import FastAPI, Request
+
+from app.api.routers import router
+
+app = FastAPI(
+    title="BaseApp",
+    description=("BaseApp"),
+    version="0.0.1",
+    docs_url="/docs",
+    redoc_url="/docs/redoc",
+)
+
+app.include_router(router)
